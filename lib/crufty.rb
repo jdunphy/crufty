8edfbdfd2b3e85c0c1e5c17aa7d17d1b6ad382ae
Jacob Dunphy <jacob.dunphy@gmail.com>
2008-06-25 10:15:19 -0700

    Cleaning up and simplifying things.

--- lib/crufty.rb
+++ lib/crufty.rb
@@ -1,21 +1,46 @@
 module Crufty
   
-  def find_methods(dir, sensitivity = 1)
-    Dir[dir.sub(/\/\Z/, '') + '/**/*.rb'].each do |file|
+  def self.find_methods(dir, sensitivity)
+    Dir.chdir(RAILS_ROOT)
+    sensitivity = (sensitivity || 1).to_i
+    Dir["#{dir.sub(/\/\Z/, '')}/**/*.rb"].each do |file|
       methods = []
       File.open(file, 'r') do |f|
         f.each_line do |l|
-          methods.push l[/def ([a-z_\?!]+)/, 1]
+          methods.push l[/def ([a-z_\?\.!]+)/, 1]
         end
       end
-      methods.compact.each do |m|
-        grep_results = `grep -r #{m} app lib`
-        if(grep_results.split("\n").length <= sensitivity.to_i)
-          puts "\n===> #{m} in #{file}"
-          puts grep_results
+      methods.compact.map {|m| CruftyMethod.new(m)}.each do |m|
+        if m.crufty?(sensitivity)
+          puts "\n===> #{m.to_s} in #{file}"
+          puts m.references
         end
       end
     end  
   end
   
+  class CruftyMethod
+    attr_accessor :name, :references
+    def initialize(prefixed_name)
+      prefixed_name.sub!(/\A\./, '')
+      if prefixed_name.index('.')
+        @prefix, @name = prefixed_name.split('.', 2)
+      else
+        @name = prefixed_name
+      end
+    end
+    
+    def to_s
+      @prefix ? "#{@prefix}.#{name}" : name
+    end
+    
+    def cruft_check
+      self.references = `grep -r #{name} app lib`.split("\n")
+    end
+    
+    def crufty?(sensitivity)
+      cruft_check unless references
+      references.length <= sensitivity
+    end
+  end
 end
--- tasks/crufty.rake
+++ tasks/crufty.rake
@@ -2,6 +2,7 @@ namespace :crufty do
   desc "Run with DIR='something' to check methods just within that location"
   task :find_methods do
     if ENV['DIR']
+      require File.dirname(__FILE__) + '/../lib/crufty'
       Crufty.find_methods(ENV['DIR'], ENV['SENSITIVITY'])
     else
       puts "DIR=<location> is required"
