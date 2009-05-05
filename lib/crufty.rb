b3d1fd23adab511aec32d2c27f871f3a348b28a3
Jacob Dunphy <jacob.dunphy@gmail.com>
2009-05-05 13:17:22 -0700

    Only do the .svn filecheck if we're actually using svn for the project.

--- lib/crufty.rb
+++ lib/crufty.rb
@@ -21,6 +21,7 @@ module Crufty
   
   class CruftyMethod
     attr_accessor :name, :references
+    
     def initialize(prefixed_name)
       prefixed_name.sub!(/\A\./, '')
       if prefixed_name.index('.')
@@ -35,12 +36,28 @@ module Crufty
     end
     
     def cruft_check
-      self.references = `grep -r #{name} app lib | grep -v \\.svn`.split("\n")
+      self.references = `grep -r #{name} app lib#{scm_based_refinement}`.split("\n")
+    end
+    
+    def scm_based_refinement
+      if self.class.scm == :svn
+        ' | grep -v \\.svn'
+      else
+        ''
+      end
     end
     
     def crufty?(sensitivity)
       cruft_check unless references
       references.length <= sensitivity
+    end
+    
+    def self.scm
+      @scm ||= if File.exists?('.svn')
+        :svn
+      else
+        "something we don't need to worry about"
+      end
     end
   end
 end
